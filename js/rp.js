667c8313a936e2184762c3470f5cbbc13c70043e
Gulimujyujyu <lucienatlinux@gmail.com>
2011-12-25 17:55:24 +0800

    one step left, TODO: validate links & track codes

--- js/rp.js
+++ js/rp.js
@@ -10,4 +10,19 @@ $(document).ready(function() {
     trigger: "hover",
     placement: "below"
   });
+  $("a.site-link-button").click(function(){
+    var ele_id = $(this).attr("href");
+    console.log(ele_id);
+    var ele_type = $(ele_id).attr("color-type");
+    $(".site-post").removeClass("site-on-chosen-col1");
+    $(".site-post").removeClass("site-on-chosen-col2");
+    $(".site-post").removeClass("site-on-chosen-col3");
+    if(ele_type == "yellow") {
+      $(ele_id).addClass("site-on-chosen-col3");
+    } else if(ele_type == "blue") {
+      $(ele_id).addClass("site-on-chosen-col2");
+    } else if(ele_type == "red") {
+      $(ele_id).addClass("site-on-chosen-col1");
+    }
+  });
 });
